db1f8e837526ea459f39199fb937399a9c66736e
afyqeyzar <norafyqe@gmail.com>
2024-01-26 11:04:41 +0000

    finish gift receive

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import GiftPage from "./pages/GiftPage/GiftPage";
 import PurchasePage from "./pages/PurchasePage/PurchasePage";
+import ReceivePage from "./pages/ReceivePage/ReceivePage";
+import FinalPage from "./pages/FinalPage/FinalPage";
+import PhonePage from "./pages/PhonePage/PhonePage";
 
 function App() {
   return (
@@ -11,7 +14,9 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/gift" element={<GiftPage />} />
         <Route path="/purchase" element={<PurchasePage />} />
+        <Route path="/phone" element={<PhonePage />} />
         <Route path="/receive" element={<ReceivePage />} />
+        <Route path="/final" element={<FinalPage />} />
       </Routes>
     </BrowserRouter>
   );
